b8bf41ed9b861a492f4acfa20546e540daaed5fe
kkulesz <kulesza.konrad@protonmail.com>
2022-05-15 20:46:12 +0200

    one more thing

--- solution_corectness_test/my_solution.py
+++ solution_corectness_test/my_solution.py
@@ -1,9 +1,17 @@
+import sys
+
 from gsp import GSP
 from main import from_file
+import consts
 
 if __name__ == '__main__':
     seqs = from_file('data.txt')
     num_of_seqs = len(seqs)
+
+    # set these values, so we get not generalized version
+    consts.MAX_GAP = sys.maxsize
+    consts.MIN_GAP = 0
+    consts.WINDOW_SIZE = 0
 
     result = GSP.run(
         seqs,
