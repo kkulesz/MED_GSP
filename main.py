66d8137d9edd00937e5b8d413a997a116bfafded
kkulesz <kulesza.konrad@protonmail.com>
2022-06-07 11:38:09 +0200

    performence tests

--- main.py
+++ main.py
@@ -1,87 +1,65 @@
 import time
-from itertools import groupby
-from typing import List
+import pandas as pd
 
-from data_classes.item import Item
-from data_classes.sequence import Sequence, Transaction
-from consts import MAX_NUMBER_OF_SEQUENCES, MIN_SUPPORT
+import utils
+from consts import MIN_SUPPORT
 from gsp import GSP
 
-
-def convert_list(raw_sequences) -> List[Sequence]:
-    result_sequences: List[Sequence] = []
-    for raw_sequence in raw_sequences:
-        transactions: List[Transaction] = []
-        for raw_transaction in raw_sequence:
-            time = raw_transaction[0]
-            items: List[Item] = []
-            for raw_item in raw_transaction[1]:
-                items.append(Item(raw_item))
-            if len(items) == 0:
-                break
-            transactions.append(Transaction(time, items))
-
-        if len(transactions) == 0:
-            break
-        result_sequences.append(Sequence(transactions))
-
-    return result_sequences
-
-
-def from_file(filepath):
-    f = open(filepath, 'r')
-    file_str = f.read()
-    ints = [int(ele) for ele in file_str.split()]
-
-    split_at = -2  # '-2' is delimiter between sequences
-    raw_sequences_ints = [list(g) for k, g in groupby(ints, lambda x: x != split_at) if k]
-    sequences = []
-    for sequence_ints in raw_sequences_ints:
-        split_at = -1  # '-1' is delimiter between transactions
-        raw_transactions_ints = [list(g) for k, g in groupby(sequence_ints, lambda x: x != split_at) if k]
-
-        transactions = []
-        transaction_time = 0
-        for raw_t in raw_transactions_ints:
-            items = list(map(lambda i: Item(i), raw_t))
-            transaction_time += 5
-            transactions.append(Transaction(transaction_time, items))
-        sequences.append(Sequence(transactions))
-
-    return sequences
-
-
-file_1 = 'data/BMS1_spmf.txt'
-file_2 = 'data/MT745584_SPMF.txt'
+file_1 = 'BMS1_spmf.txt'
+file_2 = 'MT745584_SPMF.txt'
 
 if __name__ == '__main__':
-    input_seqs = [
-        [
-            (3, [1, 2]), (4, [3]), (12, [4])
-        ],
-        [
-            (3, [1, 2]), (4, [3]), (12, [4])
-        ],
-        [
-            (3, [1, 2]), (1000, [3])
-        ],
-        [
-            (1, [1])
-        ]
-    ]
-    data = convert_list(input_seqs)
-    # data = from_file(file_2)
-    data = data[0:MAX_NUMBER_OF_SEQUENCES]
+    # input_seqs = [
+    #     [
+    #         (3, [1, 2]), (4, [3]), (12, [4])
+    #     ],
+    #     [
+    #         (3, [1, 2]), (4, [3]), (12, [4])
+    #     ],
+    #     [
+    #         (3, [1, 2]), (1000, [3])
+    #     ],
+    #     [
+    #         (1, [1])
+    #     ]
+    # ]
+    # data = utils.convert_list(input_seqs)
+    used_file = file_1
+    df = pd.DataFrame.from_dict({
+        'Number of sequences': [],
+        'Mean length': [],
+        'Median': [],
+        'Execution time': []
+    })
+    for i in [i for i in range(1, 11)]:
+        max_number_of_sequences = 1000 * i
+        data = utils.from_file(f'data/{used_file}')
+        data = data[0:max_number_of_sequences]
 
-    startTime = time.time()
-    result = GSP.run(
-        data,
-        min_supp=MIN_SUPPORT,
-        min_return_length=1
-    )
-    executionTime = (time.time() - startTime)
+        number_of_sequences = len(data)
+        mean = utils.calculate_mean_length(data)
+        median = utils.calculate_median(data)
 
-    print('RESULT:')
-    for r in result:
-        print(r)
-    print('Execution time: {:,.3f}s'.format(executionTime))
+        startTime = time.time()
+        result = GSP.run(
+            data,
+            min_supp=MIN_SUPPORT,
+            min_return_length=1
+        )
+        executionTime = (time.time() - startTime)
+
+        # print('RESULT:')
+        # for r in result:
+        #     print(r)
+        print('-'*100)
+        print(f'Number of sequences: {number_of_sequences}')
+        print(f'Mean length: {mean}')
+        print(f'Median: {median}')
+        print('Execution time: {:,.3f}s'.format(executionTime))
+        df = df.append({
+            'Number of sequences': number_of_sequences,
+            'Mean length': mean,
+            'Median': median,
+            'Execution time': executionTime
+        }, ignore_index=True)
+    df.to_csv(f'results-{used_file}.csv')
